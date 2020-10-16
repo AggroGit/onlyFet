f9b251216c54749fbc044429aea2364f91891939
Alex Ortuño <poropo97@gmail.com>
2020-10-16 16:27:00 +0200

    f

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,7 +36,7 @@ class HomeController extends Controller
       return view('success');
     }
 
-    public redirectRegister()
+    public function redirectRegister()
     {
       return redirect('/register');
     }
