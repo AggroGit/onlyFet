f550454d6c51ef097165c1d09d5fdfd627d44eb0
Alex Ortuño <poropo97@gmail.com>
2020-11-04 12:26:42 +0100

    changed pusher

--- resources/js/app.js
+++ resources/js/app.js
@@ -299,13 +299,13 @@ const store = new Vuex.Store({
         broadcaster: 'pusher',
           key:289463930,
           wsHost: "82.223.216.96",
-          wsPort: 6002,
+          wsPort: 6001,
           disableStats: true,
-           enabledTransports: ['ws','wss'],
-           encrypted:true,
+           enabledTransports: ['ws'],
+           encrypted:false,
           // enable_client_messages:true,
           cluster:'eu',
-          forceTLS:true,
+          forceTLS:false,
           authEndpoint:'/api/broadcasting/auth',
           auth: {
             headers: {
