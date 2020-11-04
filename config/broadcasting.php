93b8c6dee71976764e79cceccd7dbc1939b33f6b
Alex Ortuño <poropo97@gmail.com>
2020-11-04 18:41:21 +0100

    das

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,7 +37,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                // 'useTLS' => false,
+                'useTLS' => true,
                 'encrypted' => false,
                 'host' => env('SOCKETS_IP','34.121.39.177'),
                 'port' => 6002,
