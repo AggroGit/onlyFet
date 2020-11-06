fd0fd7e8774009e9c0b24e44a875c92b3f8c5338
Alex Ortuño <poropo97@gmail.com>
2020-11-06 12:35:23 +0100

    null

--- resources/js/components/posts/videos.vue
+++ resources/js/components/posts/videos.vue
@@ -170,7 +170,7 @@ export default {
             // webm
             // src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
           }],
-          poster: "https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg",
+          poster: "",
 
 
         // autoplay: false,
