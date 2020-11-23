3f382205a019507418c017f12adbce02d772081d
poropo97 <poropo97@gmail.com>
2020-11-23 14:41:38 +0100

    fecha

--- resources/js/components/auth/plansList.vue
+++ resources/js/components/auth/plansList.vue
@@ -242,7 +242,7 @@ Eliminar(plan) {
 },
 giveData(time) {
   var date = new Date(time.pivot.created_at);
-  return date.getFullYear()+" / "+date.getMonths() + " / "+date.getDays()
+  return date.getFullYear()+" / "+date.getMonth() + " / "+date.getDay()
 },
 giveMeRoute(noti) {
   if(noti.type == "chat") {
