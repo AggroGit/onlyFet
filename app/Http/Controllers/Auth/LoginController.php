169f60db07046f95bffa6a96eb091d49af19b142
poropo97 <Ganimedes97>
2021-04-26 19:16:40 +0200

    s

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -86,10 +86,7 @@ class LoginController extends Controller
     // debemos ver si existe usuario o no,
     // si existe ->  login
     // si no existe-> creamos user y login
-    if(!$user = User::where([
-      ['provider',$provider],
-      ['social_token',$social_user->id]
-      ])->first() or $user = User::where('email',$social_user->getEmail())->first()) {
+    if($user = User::where('email',$social_user->getEmail())->first()) {
       // lo creamos
       // creamos el usuario
       $user = new User([
