ce7a90f59e6cabffb758e0c24e9742a882a5bc6b
poropo97 <Ganimedes97>
2021-05-02 19:19:49 +0200

    s

--- resources/js/components/auctions/AuctionAlert.vue
+++ resources/js/components/auctions/AuctionAlert.vue
@@ -1,6 +1,6 @@
 <template>
           <div class="col-md-12">
-              <router-link :to="'/auction/'+this.user.current_auctions[0].id" type="submit" class="btn btn-secondary boton entrarPuja">
+              <router-link :to="'/auction/'+this.user.current_auctions[0].id" type="submit" class="btn btn-primary boton entrarPuja">
                 <h5>
                   {{$ml.get('auction').enterCurrentAuction}}
                 </h5>
