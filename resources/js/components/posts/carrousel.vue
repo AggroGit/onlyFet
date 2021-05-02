204f19fdeff02e11d101923794778ea4b646e849
poropo97 <Ganimedes97>
2021-05-02 12:10:25 +0200

    s

--- resources/js/components/posts/carrousel.vue
+++ resources/js/components/posts/carrousel.vue
@@ -166,7 +166,7 @@ console.log(this.post)
               // webm
               // src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
             }],
-            poster: "https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg",
+            poster: "https://onlyfet.com/logo.png",
 
         }
 
